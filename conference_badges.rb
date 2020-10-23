3d57058dff8a75688ad4740f916374eb2ce41210
Diana Mora <morra.dianna@gmail.com>
2020-10-23 18:33:29 -0400

    commit tree 18bf3007c5f3f76413c687220b944a567ecb5453

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
 # Write your code here.
+def badge_maker(name)
+ return "Hello, my name is #{name}."
+end
+
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+def batch_badge_creator(attendees)
+    attendees.collect do |name|
+         "Hello, my name is #{name}."
+    end
+end 
+
+
+def assign_rooms(attendees)
+    attendees.each_with_index.map do |name, index|
+        "Hello, #{name}! You'll be assigned to room #{index+1}!"
+    end
+end
+
+def printer(attendees)
+    output1 = batch_badge_creator(attendees)
+    output1.each do |x|
+      puts x 
+    end
+    output2 = assign_rooms(attendees)
+    output2.each do |x|
+      puts x 
+    end
+  end
